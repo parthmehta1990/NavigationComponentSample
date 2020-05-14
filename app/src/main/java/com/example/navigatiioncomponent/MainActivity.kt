e96a697c98ce8368a2739d9525d27bf57423d6b8
Parth Mehta <parth@id8labs.com>
2020-05-14 17:03:15 +0530

    Integrated Add Nav Drawer and Lock mechanisum in Navigation Controller components

--- app/src/main/java/com/example/navigatiioncomponent/MainActivity.kt
+++ app/src/main/java/com/example/navigatiioncomponent/MainActivity.kt
@@ -3,26 +3,52 @@ package com.example.navigatiioncomponent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.NavController
+import androidx.navigation.NavDestination
 import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.NavigationUI
 import com.example.navigatiioncomponent.databinding.ActivityMainBinding
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var drawerLayout: DrawerLayout
+    private lateinit var appBarConfiguration: AppBarConfiguration
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
        val binding=DataBindingUtil.setContentView<ActivityMainBinding>(this,R.layout.activity_main)
 
+        drawerLayout = binding.drawerLayout
+
         val navController=this.findNavController(R.id.myNavHostFragment)
+        //To link Navigation UI with actionbar by calling with navbar
+        NavigationUI.setupActionBarWithNavController(this, navController, drawerLayout)
+
+        // prevent nav gesture if not on start destination
+        navController.addOnDestinationChangedListener { nc: NavController, nd: NavDestination, args: Bundle? ->
+            if (nd.id == nc.graph.startDestination) {
+                drawerLayout.setDrawerLockMode(DrawerLayout.LOCK_MODE_UNLOCKED)
+            } else {
+                drawerLayout.setDrawerLockMode(DrawerLayout.LOCK_MODE_LOCKED_CLOSED)
+            }
+        }
+
+        NavigationUI.setupWithNavController(binding.navView, navController)
+
+        appBarConfiguration = AppBarConfiguration(navController.graph, drawerLayout)
+
+
 
         //To link Navigation UI with actionbar by calling
-        NavigationUI.setupActionBarWithNavController(this,navController)
+        //NavigationUI.setupActionBarWithNavController(this,navController)
 
     }
 
     override fun onSupportNavigateUp(): Boolean {
-       val navController=this.findNavController(R.id.myNavHostFragment)
-        return navController.navigateUp()
+        val navController = this.findNavController(R.id.myNavHostFragment)
+        return NavigationUI.navigateUp(navController,drawerLayout)
     }
 }
